95f1d768261bf8cb3bb86f42f8c68df5616d22c2
nekomaru14 <eagle.h.j.ss@gmail.com>
2021-04-07 20:32:34 +0900

    商品購入機能〜formオブジェクト作成〜

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,28 @@
+class PurchasesController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new
+  end
+
+  def new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+  def purchase_params
+    params.require(:purchase_destination).permit(:post_code, :area_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+  
+end
---
+++ app/models/purchase_destination.rb
@@ -0,0 +1,19 @@
+class PurchaseDestination
+  include ActiveModel::Model
+  attr_accessor :post_code, :area_id, :city, :address, :building_name, :phone_number, :purchase_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :city
+    validates :address
+    validates :phone_number, length: { maximum: 11 }
+    validates :purchase_id
+    validates :user_id
+    validates :item_id
+  end
+
+  def save
+    purchase = Parchase.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, area_id: area_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
