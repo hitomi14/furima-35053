fbff3263db242ec4b8dd36f65ddd43861ee555b5
nekomaru14 <eagle.h.j.ss@gmail.com>
2021-04-09 18:42:39 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if @item.user_id != current_user.id || @item.purchase.nil?
+    if @item.user_id != current_user.id || @item.purchase.present?
       redirect_to root_path
     end
   end
